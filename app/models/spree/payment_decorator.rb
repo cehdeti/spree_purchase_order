1f005df2d1871e055c275eafa657dabb2be0fd5f
Thomas Saunders <saun0063@umn.edu>
2016-08-22 12:51:05 -0500

    allow all types of documents to be uploaded

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -2,9 +2,9 @@ Spree::Payment.class_eval do
 
 	has_attached_file :po_image
 
-	has_attached_file :po_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	validates_attachment_file_name :po_image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/, /pdf\Z/, /doc\Z/, /docx\Z/]
 
-	validates_attachment_content_type :po_image, content_type: /\Aimage\/.*\z/
+	validates_attachment :po_image, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 
 
 end
