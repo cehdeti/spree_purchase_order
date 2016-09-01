2057b285ada4a078992d084922ce4d72abeb9237
Thomas Saunders <saun0063@umn.edu>
2016-09-01 09:11:26 -0500

    formatting

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,21 +1,20 @@
 Spree::Payment.class_eval do
 
-	has_attached_file :po_image
+  has_attached_file :po_image
 
-	validates_attachment_file_name :po_image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/, /pdf\Z/, /doc\Z/, /docx\Z/]
+  validates_attachment_file_name :po_image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/, /pdf\Z/, /doc\Z/, /docx\Z/]
 
-	validates_attachment :po_image, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
+  validates_attachment :po_image, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 
-	state_machine initial: :checkout do
-		after_transition to: :completed, do: :send_payment_email!
-	end
+  state_machine initial: :checkout do
+    after_transition to: :completed, do: :send_payment_email!
+  end
 
-	def send_payment_email!
+  def send_payment_email!
     if self.order.payment_state=='paid' && self.payment_method.type=='Spree::PaymentMethod::PurchaseOrder'
       logger.debug 'sending payment email!'
-			Spree::PaymentMailer.payment_complete_email(self.order).deliver_later
-		end
-
-	end
+      Spree::PaymentMailer.payment_complete_email(self.order).deliver_later
+    end
+  end
 
 end
